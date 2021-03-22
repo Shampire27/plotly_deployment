bfb688ce549d56877c6c5f5ae5ad54d934bd353b
Shampire27 <shampire27@gmail.com>
2021-03-21 23:19:34 -0400

    adding files

--- plots.js
+++ plots.js
@@ -1,3 +1,32 @@
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
+
+init();
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+    
+
 // Sort the data array using the greekSearchResults value
 data.sort(function(a, b) {
   return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
